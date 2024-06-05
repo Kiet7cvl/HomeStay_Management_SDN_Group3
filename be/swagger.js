0a922c91aea4b1d30017db772c5d28d46736fe6e
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-06-05 16:26:20 +0700

    npm install all model

---
+++ be/swagger.js
@@ -0,0 +1,12 @@
+module.exports = {
+  openapi: "1.0.0", // present supported openapi version
+  info: {
+    title: "HomestayManagement", // short title.
+    description: "HomestayManagement", //  desc.
+    version: "1.0.0", // version number
+    contact: {
+      name: "Group3", // your name
+      email: "HomestayManagement@gmail.com", // your email
+    },
+  },
+};
