45cdd3c706869dedd21d61e06d2cb198d2c9e531
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-06-13 13:40:35 +0700

    fix validation user email, user phone number

--- be/app/models/Role.model.js
+++ be/app/models/Role.model.js
@@ -14,6 +14,7 @@ const roleSchema = new Schema(
     },
     premission: [{ type: mongoose.Schema.Types.ObjectId, ref: "Permission" }],
     admins: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    owners: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     created_at: { type: Date, default: Date.now },
   },
   { collation: "roles" }
