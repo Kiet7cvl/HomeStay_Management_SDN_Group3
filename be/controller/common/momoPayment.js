a07d4141edcc0ded20e9530d9bbc18b764e3709f
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-07-10 16:34:14 +0700

    fix url thanh toan quet QR momo

--- be/controller/common/momoPayment.js
+++ be/controller/common/momoPayment.js
@@ -11,7 +11,7 @@ const createMoMoPayment = async (req, res) => {
     const redirectUrl = process.env.MOMO_REDIRECT_URL;
     const ipnUrl = process.env.MOMO_IPN_URL;
 
-    const requestType = 'payWithMethod';
+    const requestType = "captureWallet";
     const orderId = partnerCode + new Date().getTime();
     const requestId = orderId;
     const extraData = '';
