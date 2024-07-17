696ded07f83e6daee8761dee316355b6e7d493d3
giangpham16 <giangpthe161721@fpt.edu.vn>
2024-07-17 20:02:07 +0700

    upload image room

--- be/routes/room.js
+++ be/routes/room.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const roomController = require('../app/controllers/Room.controller'); // Adjust the path as needed
 const authMiddleware = require('../app/middleware/AuthJwt');
 const isAuth = authMiddleware.isAuth;
+const { upload } = require("../app/services/upload");
 
 // CRUD routes for Room
-router.post('/create', isAuth, roomController.createRoom);
+router.post('/create',upload.single("avatar") ,roomController.createRoom);
 router.get('/', roomController.getAllRooms);
 router.get('/:id', roomController.getRoomById);
 router.put('/:id', isAuth, roomController.updateRoom);
